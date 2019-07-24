de46392591af81b1ab0a26447a783c842c653661
Christoph Laubach <narkosays@gmail.com>
2019-07-24 22:49:22 +0200

    added more stuff and changed some structure

--- main.py
+++ main.py
@@ -4,16 +4,24 @@ app = Flask(__name__)
 
 
 @app.route("/")
+def base():
+    return render_template("base.html")
+
+@app.route("/index")
 def index():
     return render_template("index.html")
 
-@app.route("")
-def ():
-    return render_template()
+@app.route("/heroes")
+def heroes():
+    return render_template("heroes.html")
 
-@app.route("")
-def ():
-    return render_template()
+@app.route("/items")
+def items():
+    return render_template("items.html")
+
+@app.route("/about-me")
+def about():
+   return render_template("about.html")
 
 if __name__ == '__main__':
     app.run()
