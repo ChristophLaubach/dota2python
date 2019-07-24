aa9d28d0b1aad3d52f2348099df893a8557372b4
Christoph Laubach <narkosays@gmail.com>
2019-07-24 21:04:11 +0200

    initial commit

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("")
+def ():
+    return render_template()
+
+@app.route("")
+def ():
+    return render_template()
 
 if __name__ == '__main__':
     app.run()
